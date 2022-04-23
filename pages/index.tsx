e90af6fc20c5a0e542322f62dc418190a2addf23
erdenedavaa <erdenedavaa@users.noreply.github.com>
2022-04-23 19:54:56 +0800

    Correct name

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Inruises Mongolia</h1>
+        <h1 className={styles.title}>Incruises Mongolia</h1>
       </main>
 
       <footer className={styles.footer}>
