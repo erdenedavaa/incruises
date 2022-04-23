5d3a986c5c65e8e1c96f449ec61545a891a51800
erdenedavaa <erdenedavaa@users.noreply.github.com>
2022-04-23 22:56:48 +0800

    Custom loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  disableStaticImages: true,
+  // disableStaticImages: true,
+  loader: 'custom',
 }
 
 module.exports = nextConfig
